1e5239d3d2d34e1b7642dd7f44045290080f6589
Glen Maddern <glenmaddern@gmail.com>
2014-01-15 21:24:02 -0700

    work in progress

--- 1-color.js
+++ 1-color.js
@@ -1,20 +1,26 @@
 var Cylon = require('cylon');
- 
+
 Cylon.robot({
-  connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/cu.Sphero-RRG-AMP-SPP' },
+  connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/cu.Sphero-BPO-AMP-SPP' },
   device: { name: 'sphero', driver: 'sphero' },
- 
+
   work: function(my) {
-    var on = false;
-    every((1).second(), function() {
+    var on = false,
+      t = 0;
+    every((0.2).second(), function() {
+      var direction = (270 + Math.floor(70 * Math.cos(1/8 * Math.PI * t++))) % 360;
+      console.log([t, direction])
+
       // flash light
       if (on) {
         my.sphero.setColor("blue");
         on = false;
       } else {
-        my.sphero.setColor('black');
+        my.sphero.setColor('green');
         on = true;
       }
+
+      my.sphero.roll(60, direction);
     });
   }
 }).start();
